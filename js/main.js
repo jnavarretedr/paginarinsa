552d11f0aa9505ec2dd91dd870c9924a11bfbe6d
jnavarretedr <jnavarretedr@gmail.com>
2017-05-24 03:02:23 -0500

    Anclas + ids de cajas
    psc.

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(".submenu").click(function() {
 	$(this).children("ul").slideToggle();
 });
 
-$("ul").click(function(p) {
-	p.stopPropagation();
-});
+// $("ul").click(function(p) {
+// 	p.stopPropagation();
+// });
 
